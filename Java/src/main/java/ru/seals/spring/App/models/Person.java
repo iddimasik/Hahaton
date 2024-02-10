4baf51c6b235823921b7472523a44c56ea94807c
Antares <antarespirat@gmail.com>
2024-02-10 11:20:07 +0300

    Добавил валидацию полей при регистации

--- Java/src/main/java/ru/seals/spring/App/models/Person.java
+++ Java/src/main/java/ru/seals/spring/App/models/Person.java
@@ -3,6 +3,10 @@ package ru.seals.spring.App.models;
 import ru.seals.spring.App.util.UserStatus;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "users")
@@ -14,24 +18,33 @@ public class Person {
     private int id;
 
     @Column(name = "login")
+    @NotEmpty(message = "Логин не должен быть пустым")
+    @Size(min = 2, max = 100, message = "Логин должен быть от 2 до 100 символов длинной")
     private String login;
 
     @Column(name = "first_name")
+    @NotEmpty(message = "Имя не должно быть пустым")
+    @Size(min = 2, max = 100, message = "Имя должно быть от 2 до 100 символов длинной")
     private String firstName;
 
     @Column(name = "last_name")
+    @NotEmpty(message = "Фамилия не должна быть пустым")
+    @Size(min = 2, max = 100, message = "Фамилия должна быть от 2 до 100 символов длинной")
     private String lastName;
 
     @Column(name = "email")
+    @Email(message = "Некорректный адрес электронной почты")
     private String email;
 
     @Column(name = "password")
+    @Size(min = 8, max = 100, message = "Пароль должен быть от 8 до 100 символов длинной")
     private String password;
 
     @Column(name = "user_status")
     private String userStatus;
 
     @Column(name = "phone_number")
+    @Pattern(regexp="\\+?[0-9]+", message="Некорректный номер телефона")
     private String phoneNumber;
 
     @Column(name = "role")

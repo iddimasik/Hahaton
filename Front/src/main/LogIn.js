afe23c0306c79d24863569367c798d52018a9897
Yana <yanarudakova113@gmail.com>
2024-02-10 14:12:20 +0300

    LogIn

---
+++ Front/src/main/LogIn.js
@@ -0,0 +1,36 @@
+import React from "react";
+function LogIn(){
+    return(
+        <div>
+
+            <div className="content">
+                <form name="f" method="post" action="/process_login"   style={{color: '#D3EEFF'}} >
+                    <input type="hidden"  />
+                    <h1 className="text-2xl">Авторизация </h1>
+
+                    <div className="container">
+                        <label htmlFor="username" className="form-label">Имя пользователя: </label>
+                        <input type="text" name="username" id="username" className="form-input"/>
+                    </div>
+                    <br/>
+                    <div className="container">
+                        <label htmlFor="password" className="form-label">Пароль: </label>
+                        <input type="password" name="password" id="password" className="form-input"/>
+                        <br/>
+                    </div>
+
+                    <div className="login-button">
+                        <input type="submit" value="Login" className="log"/>
+                    </div>
+
+                    <div  style={{color: 'red'}}>
+                        Неправильные имя или пароль
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    );
+}
+
+export default LogIn;
